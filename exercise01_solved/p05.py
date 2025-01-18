b6d4734cfdb5a12db20a4d09223ca392081c6b77
Young-Seob Jeong <ysjay@chungbuk.ac.kr>
2025-01-18 01:56:16 +0000

    p05

---
+++ exercise01_solved/p05.py
@@ -0,0 +1,32 @@
+exam_one = int(input("Input exam grade one: "))
+
+exam_two = int(input("Input exam grade two: "))
+
+exam_3 = int(input("Input exam grade three: "))
+
+grades = [exam_one, exam_two, exam_3]
+sum = 0
+for grade in grades:
+  sum = sum + grade
+
+avg = sum / len(grades)
+
+if avg >= 90:
+    letter_grade = "A"
+elif avg >= 80 and avg < 90:
+    letter_grade = "B"
+elif avg > 69 and avg < 80:
+    letter_grade = "C"
+elif avg <= 69 and avg >= 60:
+    letter_grade = "D"
+else:
+    letter_grade = "F"
+
+print("Exam: " + str(grades))
+print("Average: " + str(avg))
+print("Grade: " + letter_grade)
+
+if letter_grade == "F":
+    print("Student is failing.")
+else:
+    print("Student is passing.")
